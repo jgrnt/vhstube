ab0d91cc2507d15089b292fb17a7ceb892580584
Georg Göri <georg@makeclean.org>
2015-07-21 14:16:57 +0200

    Add dummy design

---
+++ README.md
@@ -0,0 +1,9 @@
+#  vhsTube
+
+A web app to browse videos, with support for chapters.
+
+Querys an elasticsearch index as created by https://github.com/jgrnt/mkvindexer
+The videos are fetch by a separate server, that is protected via basic auth.
+Providing this basic auth credentials is also the only means of user authentication.
+
+
---
+++ lib/components/user_cred.dart
@@ -0,0 +1,41 @@
+// Copyright (c) 2015, Georg Göri. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+import 'package:polymer/polymer.dart';
+
+
+@CustomTag('user-cred')
+class UserCred extends PolymerElement {
+
+
+  /// Constructor used to create instance of MainApp.
+  UserCred.created() : super.created();
+
+
+
+  // Optional lifecycle methods - uncomment if needed.
+
+//  /// Called when an instance of main-app is inserted into the DOM.
+//  attached() {
+//    super.attached();
+//  }
+
+//  /// Called when an instance of main-app is removed from the DOM.
+//  detached() {
+//    super.detached();
+//  }
+
+//  /// Called when an attribute (such as a class) of an instance of
+//  /// main-app is added, changed, or removed.
+//  attributeChanged(String name, String oldValue, String newValue) {
+//    super.attributeChanges(name, oldValue, newValue);
+//  }
+
+//  /// Called when main-app has been fully prepared (Shadow DOM created,
+//  /// property observers set up, event listeners attached).
+ ready() {
+    super.ready();
+  }
+}
